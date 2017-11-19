b602b30ecd39a0caf28b9df27671841c8cf043f6
siddsach <ssachdeva@uchicago.edu>
2017-11-18 19:49:16 -0600

    added bash script to set server up and run experiments

---
+++ init_server.sh
@@ -0,0 +1,23 @@
+.#!/bin/bash
+
+wget https://www.python.org/ftp/python/3.6.0/Python-3.6.0.tgz
+tar xvzf Python-3.6.0.tgz
+cd Python-3.6.0
+./configure --prefix=$HOME/.local
+make
+make install
+
+cd ..
+
+git clone https://github.com/siddsach/Interpreting-Attention
+
+mv Interpreting-Attention/.bash_profile .
+source ~/.bash_profile
+
+pip3 install http://download.pytorch.org/whl/cu80/torch-0.2.0.post3-cp36-cp36m-manylinux1_x86_64.whl
+
+cd Interpreting-Attention
+pip3 install -r requirements.txt
+
+python3 get_data.py
+python3 experiments.py
