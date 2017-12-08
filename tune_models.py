ea46ede0653af5f60f2bd979172546eda82efcff
siddsach <ssachdeva@uchicago.edu>
2017-12-08 13:04:09 -0600

    trying

--- tune_models.py
+++ tune_models.py
@@ -1,25 +1,25 @@
 import GPy
 import GPyOpt
-from classifier.attention_rnn import TrainClassifier
+from pretraining.langmodel import TrainLangModel
 
 def getClassifierError(params):
-    trainer = Trainer.TrainClassifier(params)
+    trainer = TrainLangModel(params)
     trainer.train()
     return trainer.eval_loss
 
 
 vanilla_params = [
-    {"name":"lr", "type": "continuous", domain: [0, 10]},
-    {"name":"dropout", "type": "continuous", domain: [0, 0.7]}
+    {"name":"lr", "type": "continuous", "domain": [0, 10]},
+    {"name":"batch_size", "type": "discrete", "domain": [10, 100]}
 ]
 
 attn_params = vanilla_params + \
-                [{"name": "attention_dim", "type": "discrete", domain: [5, 100]},
-                 {"name": "mlp_hidden", "type": "discrete", domain: [20, 500]}]
+                [{"name": "attention_dim", "type": "discrete", "domain": [5, 100]},
+                 {"name": "mlp_hidden", "type": "discrete", "domain": [20, 500]}]
 
 
-BBX_optimizer == GPyOpt.methods.BayesianOptimization(f=getClassifierError(,# Objective function
-                                                    domain=mixed_domain,          # Box-constrains of the problem
+myBopt = GPyOpt.methods.BayesianOptimization(f=TrainLangModel,#Objective function
+                                                    domain=vanilla_params,          # Box-constrains of the problem
                                                     initial_design_numdata = 5,   # Number data initial design
                                                     acquisition_type='EI',        # Expected Improvement
                                                     exact_feval = True
@@ -27,3 +27,8 @@ BBX_optimizer == GPyOpt.methods.BayesianOptimization(f=getClassifierError(,# Obj
 
 
 
+max_iter = 20       ## maximum number of iterations
+max_time = 60       ## maximum allowed time
+eps      = 1e-6     ## tolerance, max distance between consicutive evaluations.
+
+myBopt.run_optimization(max_iter,eps=0)
