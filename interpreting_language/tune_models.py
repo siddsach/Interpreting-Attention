3a10ecc816dc34ae5d812d8454281d0ea325d8ed
siddsach <ssachdeva@uchicago.edu>
2017-12-08 16:16:56 -0600

    fixed this plez

--- interpreting_language/tune_models.py
+++ interpreting_language/tune_models.py
@@ -1,53 +1,71 @@
 import GPy
 import GPyOpt
 from pretraining.langmodel.trainer import TrainLangModel
+import pickle
 
-vanilla_params = [
-    {"name":"batch_size", "type": "discrete", "domain": [20, 40, 60, 80, 100]},
-    {"name":"seq_len", "type":"discrete", "domain":[20, 35, 50]},
-    {"name":"dropout", "type": "continuous", "domain": [0,1]},
-    {"name":"anneal", "type": "continuous", "domain": [2, 8]},
-    {"name":"num_layers", "type": "discrete", "domain": [2, 3]}
-]
+def optimize(dataset):
+    vanilla_params = [
+        {"name":"batch_size", "type": "discrete", "domain": [20, 40, 60, 80, 100]},
+        {"name":"seq_len", "type":"discrete", "domain":[20, 35, 50]},
+        {"name":"dropout", "type": "continuous", "domain": [0,1]},
+        {"name":"anneal", "type": "continuous", "domain": [2, 8]},
+        {"name":"num_layers", "type": "discrete", "domain": [2, 3]}
+    ]
 
-def getError(params):
-    settings = {}
-    for arg, value in zip(vanilla_params, params[0]):
-        value = value.item()
-        if arg["type"] == "discrete":
-            value = int(value)
+    def getError(params):
+        settings = {}
+        for arg, value in zip(vanilla_params, params[0]):
+            value = value.item()
+            if arg["type"] == "discrete":
+                value = int(value)
 
-        settings[arg['name']] = value
+            settings[arg['name']] = value
 
-    print("SETTINGS FOR THIS RUN")
-    print(settings)
-    trainer = TrainLangModel(**settings)
-    trainer.train()
-    return trainer.best_eval_perplexity
+        settings["data"] = dataset
+        settings["num_epochs"] = 1
 
-def test(params):
-    print('testing')
-    settings = {arg["name"]: value for arg, value in zip(vanilla_params, params[0])}
-    out = settings['lr']**2 + 5 * settings['batch_size']
-    return out
+        print("SETTINGS FOR THIS RUN")
+        print(settings)
+        trainer = TrainLangModel(**settings)
+        trainer.train()
+        return trainer.best_eval_perplexity
 
-
-attn_params = vanilla_params + \
-                [{"name": "attention_dim", "type": "discrete", "domain": [5, 100]},
-                 {"name": "mlp_hidden", "type": "discrete", "domain": [20, 500]}]
-
-
-myBopt = GPyOpt.methods.BayesianOptimization(f=getError,#Objective function
-                                                    domain=vanilla_params,          # Box-constrains of the problem
-                                                    initial_design_numdata = 5,   # Number data initial design
-                                                    acquisition_type='EI',        # Expected Improvement
-                                                    exact_feval = True
-                                                )
+    def test(params):
+        print('testing')
+        settings = {arg["name"]: value for arg, value in zip(vanilla_params, params[0])}
+        out = settings['lr']**2 + 5 * settings['batch_size']
+        return out
 
 
+    attn_params = vanilla_params + \
+                    [{"name": "attention_dim", "type": "discrete", "domain": [5, 100]},
+                     {"name": "mlp_hidden", "type": "discrete", "domain": [20, 500]}]
 
-max_iter = 20       ## maximum number of iterations
-max_time = 3.5 * 60 * 60 ## maximum allowed time
-eps      = 0.001 ## tolerance, max distance between consicutive evaluations.
 
-myBopt.run_optimization(max_time,eps)
+    myBopt = GPyOpt.methods.BayesianOptimization(f=getError,#Objective function
+                                                        domain=vanilla_params,          # Box-constrains of the problem
+                                                        initial_design_numdata = 5,   # Number data initial design
+                                                        acquisition_type='EI',        # Expected Improvement
+                                                        exact_feval = True
+                                                    )
+
+
+
+    max_iter = 1 ## maximum number of iterations
+    max_time = 3.5 * 60 * 60 ## maximum allowed time
+    eps      = 0.001 ## tolerance, max distance between consicutive evaluations.
+
+    myBopt.run_optimization(max_time = max_time, max_iter = max_iter)
+    return myBopt.x_opt, myBopt.fx_opt
+
+
+best = {}
+for dataset in ['ptb', 'wikitext']:
+    best[dataset] = {}
+
+    best_params, best_loss = optimize(dataset)
+
+    best[dataset]['params'] = best_params
+    best[dataset]['loss'] = best_loss
+
+pickle.dump(best, open('params.pickle', 'wb'))
