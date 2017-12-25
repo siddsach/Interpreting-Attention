5e2bb9f39e5f7dbb535bf5818063a9e84058bdbd
siddsach <ssachdeva@uchicago.edu>
2017-12-26 00:41:51 +0800

    got visualize working

--- interpreting_language/visualize.py
+++ interpreting_language/visualize.py
@@ -1,4 +1,3 @@
-#Visualize function taken from https://github.com/NVIDIA/sentiment-discovery
 import math
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -6,7 +5,7 @@ import numpy as np
 
 
 
-def plot_neuron_heatmap(words, wordvalues, savename=None, negate=False, cell_height=.325, cell_width=.15):
+def get_heatmap(words, wordvalues, ax = None, savename=None, negate=False, cell_height=.325, cell_width=.15):
     assert len(words) == len(wordvalues)
     text, values = [], []
     for i, word in enumerate(words):
@@ -26,20 +25,45 @@ def plot_neuron_heatmap(words, wordvalues, savename=None, negate=False, cell_hei
     text = text.reshape(-1, n_limit)
     mask = mask.reshape(-1, n_limit)
     num_rows = len(values) + 1
-    plt.figure(figsize=(cell_width*n_limit, cell_height*num_rows))
-    hmap=sns.heatmap(values, annot=text, mask=mask, fmt='', vmin=-1, vmax=1, cmap='RdYlGn',
-                    xticklabels=False, yticklabels=False, cbar=False)
-    plt.tight_layout()
+    hmap = None
+    if ax is None:
+        plt.figure(figsize=(cell_width*n_limit, cell_height*num_rows))
+        hmap=sns.heatmap(values, annot=text, mask=mask, fmt='', vmin=-1, vmax=1, cmap='RdYlGn',xticklabels=False, yticklabels=False, cbar=False)
+        plt.tight_layout()
+    else:
+        hmap=sns.heatmap(values, annot=text, mask=mask, fmt='', vmin=-1, vmax=1, cmap='RdYlGn',xticklabels=False, yticklabels=False, cbar=False, ax = ax)
+
     if savename is not None:
         plt.savefig(savename)
-    # clear plot for next graph since we returned `hmap`
-    plt.clf()
     return hmap
+
+#LIST OF SENTENCES (LIST OF WORDS), LIST OF LIST OF VALS
+def plot_attn(texts, vals, savepath = None):
+    fig, axes= plt.subplots(len(texts))
+    for i in range(len(sentences)):
+        get_heatmap(datatext[i], datavals[i], ax = axes[i])
+    if savepath is None:
+        plt.show()
+    else:
+        plt.savefig(savepath)
 
 if __name__ == '__main__':
 
-    text = 'This is a longer sentence to get a better idea! of how it deals? with DIFFERNT STuff'
-    words = text.split(" ")
-    vals = [(i - (len(words) / 2))/(len(words)/2) for i in range(len(words))]
+    text = 'This is a longer sentence to get '
+    two = 'a better idea! of how it deals? with DIFFERNT STuff'
+    sentences = 2* [text, two, text, two]
 
-    plot_neuron_heatmap(words, vals, savename = 'viz_attn.png')
+    datatext, datavals = [], []
+    for text in sentences:
+        words = text.split(" ")
+        datatext.append(words)
+        vals = [(i - (len(words) / 2))/(len(words)/2) for i in range(len(words))]
+        datavals.append(vals)
+    plot_attn(datatext, datavals)
+
+    '''
+    fig.axes.append(ax1)
+    fig.axes.append(ax2)
+    plt.show()
+    '''
+
