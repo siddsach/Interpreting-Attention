d6594422d9f15e1ad88a62d4d218b921a459e335
siddsach <ssachdeva@uchicago.edu>
2017-12-25 18:35:33 +0800

    m

--- interpreting_language/tune_models.py
+++ interpreting_language/tune_models.py
@@ -2,23 +2,56 @@ import GPyOpt
 import argparse
 from time import time
 
+# TRAIN LANGUAGE MODELS
+LANGMODEL_CHOICES = [
+    {"name":"lr", "type": "continuous", "domain":[0.00005, 0.005]},
+    {"name":"dropout", "type": "continuous", "domain": [0,1]},
+    {"name":"anneal", "type": "continuous", "domain": [2, 8]},
+]
+
+
+CLASSIFIER_CHOICES = [
+                {"name":"lr", "type": "continuous", "domain":[0.00005, 0.005]},
+                {"name":"rnn_dropout", "type": "continuous", "domain": [0,1]},
+                {"name":"dropout", "type": "continuous", "domain": [0,1]},
+                {"name":"l2", "type": "continuous", "domain": [0, 1]},
+            ]
+
+ATTNCLF_CHOICES = [
+                {"name": "attention_dim", "type": "attention_dim", "domain": [5, 100]}
+            ]
 
 class Optimizer:
-    def __init__(self, dataset, vectors, tune_wordvecs, wordvec_dim, choices, TrainerClass, timelimit, num_layers, \
+    def __init__(self, dataset, model, vectors, wordvec_dim, tune_wordvecs, tune_attn, num_layers, timelimit, \
             batch_size, seq_len):
 
         self.start_time = time()
 
-        print('Building Bayesian Optimizer for \n data:{} \n choices:{}'.format(dataset, choices))
+        self.choices = None
+        self.TrainerClass= None
 
-        self.choices = choices
+        if model == 'langmodel':
+            self.choices = LANGMODEL_CHOICES
+            from pretraining.langmodel.trainer import TrainLangModel
+            self.TrainerClass = TrainLangModel
+        elif model == 'classifier' or model == 'keyvalattn':
+            self.choices = CLASSIFIER_CHOICES
+            from classifier.attention_rnn.trainer import TrainClassifier
+            self.TrainerClass = TrainClassifier
+        elif model == 'mlpattn':
+            self.choices = CLASSIFIER_CHOICES + ATTNCLF_CHOICES
+            from classifier.attention_rnn.trainer import TrainClassifier
+            self.TrainerClass = TrainClassifier
+
+        print('Building Bayesian Optimizer for \n data:{} \n choices:{}'.format(dataset, self.choices))
+
         self.dataset = dataset
         self.best_loss = 100000
-        self.TrainerClass = TrainerClass
         self.model = None
         self.vectors = vectors
         self.num_layers = num_layers
         self.tune_wordvecs = tune_wordvecs
+        self.tune_attn = tune_attn
         self.wordvec_dim = wordvec_dim
         self.best_accuracy = -10000000
         self.batch_size = batch_size
@@ -29,7 +62,7 @@ class Optimizer:
         self.model = None
 
         myBopt = GPyOpt.methods.BayesianOptimization(f=self.getError,#Objective function
-                                                            domain=choices,          # Box-constrains of the problem
+                                                            domain=self.choices,          # Box-constrains of the problem
                                                             initial_design_numdata = 5,   # Number data initial design
                                                             acquisition_type='EI',        # Expected Improvement
                                                             exact_feval = True
@@ -65,12 +98,17 @@ class Optimizer:
         settings["tune_wordvecs"] = self.tune_wordvecs
         settings["wordvec_dim"] = self.wordvec_dim
         settings["batch_size"] = self.batch_size
-        settings["seq_len"] = self.seq_len
+
+        if self.model == 'langmodel':
+            settings["seq_len"] = self.seq_len
+        elif self.model == ['keyvalattn']:
+            settings["tune_attn"] = self.tune_attn
 
         print("SETTINGS FOR THIS RUN")
         print(settings)
         trainer = self.TrainerClass(**settings)
-        trainer.train()
+        optimizer = trainer.start_train()
+        trainer.train(optimizer)
 
         if trainer.best_accuracy > self.best_accuracy:
             print('Improved accuracyfrom {} to {}'.format(self.best_accuracy, trainer.best_accuracy))
@@ -97,76 +135,42 @@ class Optimizer:
 
 
 
-# TRAIN LANGUAGE MODELS
-LANGMODEL_CHOICES = [
-    {"name":"lr", "type": "continuous", "domain":[0.00005, 0.005]},
-    {"name":"dropout", "type": "continuous", "domain": [0,1]},
-    {"name":"anneal", "type": "continuous", "domain": [2, 8]},
-]
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Tuning Hyperparameters')
+    parser.add_argument('--data', type=str, default='ptb',
+                        help='dataset')
+    parser.add_argument('--model', type=str, default='classifier',
+                        help='type of model to train')
+    parser.add_argument('--vectors', type=str, default='',
+                        help='vectors to use')
+    parser.add_argument('--num_layers', type=int, default=1,
+                        help='vectors to use')
+    parser.add_argument('--tune_wordvecs', type=str, default='True',
+                        help='whether to tune wordvecs')
+    parser.add_argument('--batch_size', type=int, default=20,
+                        help='wordvec_dim')
+    parser.add_argument('--wordvec_dim', type=int, default=200,
+                        help='wordvec_dim')
+    parser.add_argument('--timelimit', type=int, default=3*60*60,
+                        help='wordvec_dim')
+    parser.add_argument('--seq_len', type=int, default=35,
+                        help='wordvec_dim')
+    parser.add_argument('--tune_attn', type=str, default='true',
+                        help='wordvec_dim')
 
-CLASSIFIER_CHOICES = [
-                {"name":"lr", "type": "continuous", "domain":[0.00005, 0.005]},
-                {"name":"rnn_dropout", "type": "continuous", "domain": [0,1]},
-                {"name":"dropout", "type": "continuous", "domain": [0,1]},
-                {"name":"l2", "type": "continuous", "domain": [0, 1]},
-            ]
+    args = parser.parse_args()
+    def str2bool(v):
+        if v.lower() in ('yes', 'true', 't', 'y', '1'):
+            return True
+        elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+            return False
+        else:
+            raise argparse.ArgumentTypeError('Boolean value expected.')
+    args.tune_wordvecs = str2bool(args.tune_wordvecs)
+    args.tune_attn = str2bool(args.tune_attn)
 
-ATTNCLF_CHOICES = [
-                {"name": "attention_dim", "type": "discrete", "domain": [5, 100]},
-                {"name": "mlp_hidden", "type": "discrete", "domain": [20, 500]}
-            ]
+    Optimizer(dataset = args.data, model = args.model, vectors = args.vectors, wordvec_dim = args.wordvec_dim,
+                 tune_wordvecs = args.tune_wordvecs, tune_attn = args.tune_attn, num_layers = args.num_layers, timelimit = args.timelimit,
+                 batch_size = args.batch_size, seq_len = args.seq_len)
 
-MLPATTN_CHOICES = []
-
-KEYVALATTN_CHOICES = []
-
-def tuneModels(dataset, model, vectors, wordvec_dim, tune_wordvecs, num_layers, batch_size, seq_len):
-    choices = None
-    trainerclass = None
-
-    if model == 'langmodel':
-        choices = LANGMODEL_CHOICES
-        from pretraining.langmodel.trainer import TrainLangModel
-        trainerclass = TrainLangModel
-    elif model == 'classifier':
-        choices = CLASSIFIER_CHOICES
-        from classifier.attention_rnn.trainer import TrainClassifier
-        trainerclass = TrainClassifier
-    elif model == 'mlpattn':
-        choices = MLPATTN_CHOICES
-        from classifier.attention_rnn.trainer import TrainClassifier
-        trainerclass = TrainClassifier
-    elif model == 'keyvalattn':
-        choices = KEYVALATTN_CHOICES
-        from classifier.attention_rnn.trainer import TrainClassifier
-        trainerclass = TrainClassifier
-
-
-    max_time = 30# * 60 * 60 ## maximum allowed time
-    opt = Optimizer(dataset, vectors, tune_wordvecs, wordvec_dim, choices, trainerclass, max_time, num_layers, batch_size, seq_len)
-
-
-parser = argparse.ArgumentParser(description='Tuning Hyperparameters')
-parser.add_argument('--data', type=str, default='ptb',
-                    help='dataset')
-parser.add_argument('--model', type=str, default='classifier',
-                    help='type of model to train')
-parser.add_argument('--vectors', type=str, default='',
-                    help='vectors to use')
-parser.add_argument('--num_layers', type=int, default=1,
-                    help='vectors to use')
-parser.add_argument('--tune_wordvecs', type=bool, default=True,
-                    help='whether to tune wordvecs')
-parser.add_argument('--batch_size', type=int, default=20,
-                    help='wordvec_dim')
-parser.add_argument('--wordvec_dim', type=int, default=200,
-                    help='wordvec_dim')
-parser.add_argument('--seq_len', type=int, default=35,
-                    help='wordvec_dim')
-
-args = parser.parse_args()
-
-tuneModels(args.data, model = args.model, vectors = args.vectors, wordvec_dim = args.wordvec_dim,
-             tune_wordvecs = args.tune_wordvecs, num_layers = args.num_layers, batch_size = args.batch_size,
-             seq_len = args.seq_len)
--- interpreting_language/visualize.py
+++ interpreting_language/visualize.py
@@ -4,26 +4,42 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_neuron_heatmap(text, values, savename=None, negate=False, cell_height=.325, cell_width=.15):
-	n_limit = 74
-	num_chars = len(text)
-	total_chars = math.ceil(num_chars/float(n_limit))*n_limit
-	mask = np.array([0]*num_chars + [1]*(total_chars-num_chars))
-	text = np.array(text+[' ']*(total_chars-num_chars))
-	values = np.array(values+[0]*(total_chars-num_chars))
-	if negate:
-		values *= -1
 
-	values = values.reshape(-1, n_limit)
-	text = text.reshape(-1, n_limit)
-	mask = mask.reshape(-1, n_limit)
-	num_rows = len(values)
-	plt.figure(figsize=(cell_width*n_limit, cell_height*num_rows))
-	hmap=sns.heatmap(values, annot=text, mask=mask, fmt='', vmin=-1, vmax=1, cmap='RdYlGn',
-					xticklabels=False, yticklabels=False, cbar=False)
-	plt.tight_layout()
-	if savename is not None:
-		plt.savefig(savename)
-	# clear plot for next graph since we returned `hmap`
-	plt.clf()
-	return hmap
+
+def plot_neuron_heatmap(words, wordvalues, savename=None, negate=False, cell_height=.325, cell_width=.15):
+    assert len(words) == len(wordvalues)
+    text, values = [], []
+    for i, word in enumerate(words):
+        text += list(word) + [" "]
+        values += (len(word) + 1) * [wordvalues[i]]
+
+    n_limit = 74
+    num_chars = len(text)
+    total_chars = math.ceil(num_chars/float(n_limit))*n_limit
+    mask = np.array([0]*num_chars + [1]*(total_chars-num_chars))
+    text = np.array(text+[' ']*(total_chars-num_chars))
+    values = np.array(values+[0]*(total_chars-num_chars))
+    if negate:
+        values *= -1
+
+    values = values.reshape(-1, n_limit)
+    text = text.reshape(-1, n_limit)
+    mask = mask.reshape(-1, n_limit)
+    num_rows = len(values) + 1
+    plt.figure(figsize=(cell_width*n_limit, cell_height*num_rows))
+    hmap=sns.heatmap(values, annot=text, mask=mask, fmt='', vmin=-1, vmax=1, cmap='RdYlGn',
+                    xticklabels=False, yticklabels=False, cbar=False)
+    plt.tight_layout()
+    if savename is not None:
+        plt.savefig(savename)
+    # clear plot for next graph since we returned `hmap`
+    plt.clf()
+    return hmap
+
+if __name__ == '__main__':
+
+    text = 'This is a longer sentence to get a better idea! of how it deals? with DIFFERNT STuff'
+    words = text.split(" ")
+    vals = [(i - (len(words) / 2))/(len(words)/2) for i in range(len(words))]
+
+    plot_neuron_heatmap(words, vals, savename = 'viz_attn.png')
